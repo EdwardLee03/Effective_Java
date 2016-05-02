1df757fbea0acdf322ce699a748dec9024ff532d
Bert Lee <bert825_work@163.com>
2016-05-02 20:43:03 +0800

    第14条：私有域+getter/setter——在公有类中使用访问方法 而非公有域

---
+++ src/main/java/c4/g14/Point.java
@@ -0,0 +1,45 @@
+package c4.g14;
+
+/**
+ * 在公有类中使用访问方法 而非公有域。
+ * <p/>
+ * Encapsulation of data by accessor methods and mutators.
+ * 数据访问方法和赋值方法的封装。
+ * <p/>
+ * JDK 的反例教材：{@link java.awt.Point}
+ *
+ * @author xingle
+ * @since 2016年05月02日 20:30
+ */
+class Point {
+
+    // 私有域
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // 公有访问方法（getter）
+    public double getX() {
+        return x;
+    }
+
+    // 公有设值方法（setter）
+    public Point setX(double x) {
+        this.x = x;
+        return this;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Point setY(double y) {
+        this.y = y;
+        return this;
+    }
+
+}
